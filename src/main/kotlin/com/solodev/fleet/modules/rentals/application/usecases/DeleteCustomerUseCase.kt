b7677d8a6d3f25c0467272729b206ab57fe4c259
farsuller <farsuller2020@gmail.com>
2026-05-03 11:50:21 +0800

    fix lint error

--- src/main/kotlin/com/solodev/fleet/modules/rentals/application/usecases/DeleteCustomerUseCase.kt
+++ src/main/kotlin/com/solodev/fleet/modules/rentals/application/usecases/DeleteCustomerUseCase.kt
@@ -3,8 +3,8 @@ package com.solodev.fleet.modules.rentals.application.usecases
 import com.solodev.fleet.modules.rentals.domain.model.CustomerId
 import com.solodev.fleet.modules.rentals.domain.repository.CustomerRepository
 
-class DeleteCustomerUseCase(private val repository: CustomerRepository) {
-    suspend fun execute(id: String): Boolean {
-        return repository.deleteById(CustomerId(id))
-    }
+class DeleteCustomerUseCase(
+    private val repository: CustomerRepository,
+) {
+    suspend fun execute(id: String): Boolean = repository.deleteById(CustomerId(id))
 }
